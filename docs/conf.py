8c7b34fb3f83ebc6d683fe1fd1bc0e638f827270
PanoPepino <panocuenta@gmail.com>
2025-01-27 18:02:41 +0900

    try

--- docs/conf.py
+++ docs/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath("../mtheoretical/src"))
+sys.path.insert(0, os.path.abspath("../mtheoretical"))
 
 project = 'mtheoretical'
 copyright = '2025, Pano'
